7da6c5367e46e7a5d0ad6fb55e6eebf1d3bc9c91
maheshsingh-74 <maheshbeniwal74@gmail.com>
2026-09-09 02:27:17 +0530

    Add files via upload

---
+++ question11.sql
@@ -0,0 +1,9 @@
+SELECT
+CASE
+    WHEN G.Grade < 8 THEN NULL ELSE S.Name
+END
+, G.Grade, S.Marks
+FROM STUDENTS S
+INNER JOIN GRADES G
+ON S.Marks BETWEEN G.Min_Mark AND G.Max_Mark
+ORDER BY G.Grade DESC, S.Name,S.Marks;
